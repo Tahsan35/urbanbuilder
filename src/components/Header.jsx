0561051a46d89585339ace9bdc0ace6deaf9be6a
Tahan Shakil <tahsanknmas14@gmail.com>
2025-03-18 02:17:50 +0600

    added header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,7 @@
 "use client";
+import { RiArrowRightUpLine } from "react-icons/ri";
 import { Link } from "react-scroll";
+import Logo from "./Logo";
 const links = [
   { name: "home", path: "home" },
   { name: "about", path: "about" },
@@ -11,25 +13,45 @@ const Header = () => {
   return (
     <header className="bg-black py-4 text-gray-400 sticky top-0">
       <div className="container mx-auto">
-        <ul className="flex gap-11 text-white">
-          {links.map((link, index) => {
-            return (
-              <li key={index}>
-                <Link
-                  to={link.path}
-                  smooth={true}
-                  and
-                  spy={true}
-                  className="cursor-pointer"
-                  //activeClass="text-gray-400"
-                  activeClass="text-accent"
-                >
-                  {link.name}
-                </Link>
-              </li>
-            );
-          })}
-        </ul>
+        <div className="flex items-center justify-between w-full">
+          {/* logo */}
+          <Logo />
+
+          {/* nav and btn */}
+          <nav className="hidden xl:flex items-center gap-10">
+            <ul className="flex">
+              {links.map((link, index) => {
+                return (
+                  <li
+                    key={index}
+                    className="cursor-pointer text-white text-sm uppercase font-medium tracking-[1.2px] after:content-['/'] after:px-4 last:after:content-none"
+                  >
+                    <Link
+                      to={link.path}
+                      smooth={true}
+                      and
+                      spy={true}
+                      //className="cursor-pointer"
+                      //activeClass="text-gray-400"
+                      activeClass="text-accent"
+                    >
+                      {link.name}
+                    </Link>
+                  </li>
+                );
+              })}
+            </ul>
+            {/* btn */}
+            <button className="w-[200px] h-[54px] py-[5px] pl-[10px] pr-[5px] flex items-center justify-center min-w-[200px] group bg-white rounded-l-2xl">
+              <div className="flex-1 text-center tracking-[1.2px] font-bold text-black text-sm font-serif uppercase">
+                Get a quote
+              </div>
+              <div className="w-11 h-11 bg-black flex items-center justify-center">
+                <RiArrowRightUpLine className="text-white text-xl group-hover:rotate-45 transition-all duration-200" />
+              </div>
+            </button>
+          </nav>
+        </div>
       </div>
     </header>
   );
