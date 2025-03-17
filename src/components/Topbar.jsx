ef7cc3b432e2c3dc078529042e906a6e395ac601
Tahan Shakil <tahsanknmas14@gmail.com>
2025-03-18 00:35:07 +0600

    added topbar

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -1,5 +1,39 @@
+import { RiPhoneFill, RiMailFill } from "react-icons/ri";
+import Socials from "./Socials";
+
 const Topbar = () => {
-  return <div id="home">Topbar</div>;
+  return (
+    <section
+      id="home"
+      className="text-gray-400 py-4 xl:h-16 xl:py-1 bg-gradient-to-t from-[#ffc221] to-[#ffd76e] flex items-center"
+    >
+      <div className="container mx-auto">
+        {/* phone,mail,social */}
+        <div className="flex flex-col lg:flex-row items-center justify-between gap-6 w-full">
+          <div className="hidden lg:flex items-center gap-8">
+            {/* phone */}
+            <div className="flex  items-center gap-3">
+              <div className="w-8 h-8 bg-black text-white flex items-center justify-center">
+                <RiPhoneFill />
+              </div>
+              <p className="font-medium text-gray-400">01623443703</p>
+            </div>
+            {/* mail */}
+            <div className="flex items-center gap-3">
+              <div className="w-8 h-8 bg-black text-white flex items-center justify-center">
+                <RiMailFill />
+              </div>
+              <p className="font-medium text-gray-400">
+                md.shakil.developer@gmail.com
+              </p>
+            </div>
+          </div>
+          {/* social */}
+          <Socials />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Topbar;
