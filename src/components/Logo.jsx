cc2843ade2d848786e37c59661d627920b92f9e0
Tahan Shakil <tahsanknmas14@gmail.com>
2025-03-18 02:41:43 +0600

    added header logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,5 +1,13 @@
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+
 const Logo = () => {
-  return <div>Logo</div>;
+  return (
+    <Link href="/">
+      <Image src="/assets_2/logo.svg" width={230} height={48} alt="logo" />
+    </Link>
+  );
 };
 
 export default Logo;
