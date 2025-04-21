c3c0d7bd2f018e811498c14d3c8d0924b8fd4353
Tahan Shakil <tahsanknmas14@gmail.com>
2025-04-21 21:04:37 +0600

    feat(components): add Work and Services components

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,8 +1,193 @@
+"use client";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import Image from "next/image";
+import Button from "./Button";
+
+import {
+  PiWallFill,
+  PiPaintRollerFill,
+  PiWrenchFill,
+  PiUserGearFill,
+} from "react-icons/pi";
+import { useState } from "react";
+import Pretitle from "./Pretitle";
+
+const serviceData = [
+  {
+    name: "Construction",
+    icon: <PiWallFill />,
+    title: "Construction Services",
+    description:
+      "Professional construction services for residential and commercial projects.",
+    serviceList: [
+      "Residential construction",
+      "Structural engineering",
+      "Commercial construction",
+      "Building renovation",
+      "Interior design",
+      "Electrical and plumbing services",
+    ],
+    thumbs: [
+      {
+        url: "/assets_2/img/services/thumb-1.jpg",
+      },
+      {
+        url: "/assets_2/img/services/thumb-2.jpg",
+      },
+    ],
+  },
+  {
+    name: "Renovation",
+    icon: <PiPaintRollerFill />,
+    title: "Renovation Services",
+    description:
+      "Complete renovation and remodeling services for your property.",
+    serviceList: [
+      "Kitchen Remodel",
+      "Basement Finish",
+      "Flooring",
+      "carpentry",
+      "painting",
+    ],
+    thumbs: [
+      {
+        url: "/assets_2/img/services/thumb-3.jpg",
+      },
+      {
+        url: "/assets_2/img/services/thumb-4.jpg",
+      },
+    ],
+  },
+  {
+    name: "Restoration",
+    icon: <PiWrenchFill />,
+    title: "Restoration Services",
+    description:
+      "Bringing damaged or aging structures back to life. We specialize in restoring historical landmarks, fire-damaged buildings, and water-damaged properties with meticulous care.",
+    serviceList: [
+      "Historic Restore",
+      "Water Damage",
+      "Fire Repair",
+      "Structural Fix",
+      "Mold Removal",
+      "Roof Restore",
+    ],
+    thumbs: [
+      {
+        url: "/assets_2/img/services/thumb-4.jpg",
+      },
+      { url: "/assets_2/img/services/thumb-5.jpg" },
+    ],
+  },
+  {
+    name: "Consulting",
+    icon: <PiUserGearFill />,
+    title: "Consulting Services",
+    description: "Expert guidance and insights for your construction projects.",
+    serviceList: [
+      "Project Plans",
+      "Costing",
+      "Site Management",
+      "Safety",
+      "Roof Restore",
+    ],
+    thumbs: [
+      {
+        url: "/assets_2/img/services/thumb-1.jpg",
+      },
+      {
+        url: "/assets_2/img/services/thumb-3.jpg",
+      },
+    ],
+  },
+];
 const Services = () => {
+  const [activeTab, setActiveTab] = useState("construction");
+
   return (
-    <div id="services" className="h-screen bg-green-200">
-      Services
-    </div>
+    <section className="pt-16 xl:pt-32" id="services">
+      <div className="container mx-auto">
+        <div className="text-center max-w-[540px] mx-auto mb-20">
+          <Pretitle text="Our Services" center />
+          <h2 className="h2 mb-3">Solutions We Provide</h2>
+          <p className="mb-11 max-w-[480px] mx-auto">
+            Offering tailored construction solutions, from planning to
+            completion, with a focus on quality and innovation.
+          </p>
+        </div>
+        {/* tabs */}
+        <Tabs
+          defaultValue="Construction"
+          onValueChange={(value) => setActiveTab(value)}
+          className="flex flex-col xl:flex-row w-full gap-[30px]"
+        >
+          <TabsList className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-1 gap-[30px] rounded-none p-0 w-full h-full xl:w-[345px] bg-transparent">
+            {serviceData.map((item) => {
+              return (
+                <TabsTrigger
+                  key={item.name}
+                  value={item.name}
+                  className="w-full rounded-none h-[100px] flex items-center relative shadow-custom p-0 outline-none"
+                >
+                  <div
+                    className={`w-[100px] h-[100px] flex items-center justify-center absolute left-0 ${
+                      activeTab === item.name
+                        ? "bg-black text-white"
+                        : "bg-accent text-black"
+                    }`}
+                  >
+                    <div>{item.icon}</div>
+                  </div>
+                  <div className="uppercase text-base font-semibold tracking-[.6px] w-[100px] ml-16">
+                    {item.name}
+                  </div>
+                </TabsTrigger>
+              );
+            })}
+          </TabsList>
+          {/* tabs content */}
+          <div className="flex-1 bg-white shadow-custom h-[490px] p-[30px]">
+            {serviceData.map((item) => (
+              <TabsContent key={item.name} value={item.name} className="m-0">
+                <div className="flex flex-col md:flex-row gap-[30px]">
+                  {/* image */}
+                  <div className="flex md:flex-col gap-5 xl:gap-[30px]">
+                    {item.thumbs.map((thumb, index) => (
+                      <div
+                        key={index}
+                        className=" relative w-[140px] xl:w-[200px] h-[140px] xl:h-[200px]"
+                      >
+                        <Image src={thumb.url} fill alt="" />
+                      </div>
+                    ))}
+                  </div>
+                  {/* text and button */}
+                  <div>
+                    <div>
+                      <h3 className="h3 mb-6">{item.title}</h3>
+                      <p className="mb-10">{item.description}</p>
+                      {/* service list */}
+                      <ul className="grid grid-cols-2 gap-4 mb-12">
+                        {item.serviceList.map((service, index) => (
+                          <li key={index} className="flex items-center gap-4">
+                            <div className="w-[6px] h-[6px] bg-accent"></div>
+                            <div className="capitalize font-medium text-black">
+                              {service}
+                            </div>
+                          </li>
+                        ))}
+                      </ul>
+                      {/* btn */}
+                      <Button text="read more" />
+                    </div>
+                  </div>
+                </div>
+              </TabsContent>
+            ))}
+          </div>
+        </Tabs>
+      </div>
+    </section>
   );
 };
 
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,7 +1,84 @@
+import { RiArrowRightUpLine, RiCheckboxCircleFill } from "react-icons/ri";
+import Pretitle from "./Pretitle";
+import Image from "next/image";
+import Link from "next/link";
+
+// Add project data
+const workData = [
+  {
+    img: "/assets_2/img/work/restoration.jpg",
+    category: "restoration",
+    description:
+      "Residential restoration project in downtown area with modern design",
+    href: "",
+  },
+  {
+    img: "/assets_2/img/work/construction.jpg",
+    category: "construction",
+    description:
+      " Construction project in downtown area with modern design.we are provide best services to our clients.",
+  },
+  {
+    img: "/assets_2/img/work/renovation.jpg",
+    category: "renovation",
+    description: "Renovation project in downtown area with modern design ",
+  },
+  {
+    img: "/assets_2/img/work/consulting.jpg",
+    category: "consulting",
+    description: "Preserving architectural heritage with modern techniques",
+  },
+];
+
 const Work = () => {
   return (
-    <div id="projects" className="h-screen bg-yellow-200">
-      Work
+    <div className="pt-16 xl:pt-32" id="projects">
+      <div className="container mx-auto">
+        <div className="text-center max-w-[540px] mx-auto xl:mb-20">
+          <Pretitle text="Our work" center />
+          <h2 className="h2 mb-3">Discover Our Projects</h2>
+          <p className="mb-11 max-w-[480px] mx-auto">
+            Providing expert services designer to deliver quality and innovation
+            in every project we undertake.
+          </p>
+        </div>
+      </div>
+      {/* Project grid */}
+      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4">
+        {workData.map((item, index) => {
+          return (
+            <div
+              key={index}
+              className="w-full h-[492px] flex-1 relative overflow-hidden group flex justify-center"
+            >
+              <Image
+                src={item.img}
+                fill
+                className="object-cover"
+                alt=""
+                quality={100}
+              />
+              <div className="w-[90%] h-[84px] bg-black absolute bottom-4 flex justify-between items-center text-white md:translate-y-[108px] md:group-hover:translate-y-0 transition-all duration-500">
+                <div className="pl-8">
+                  <h4 className="text-white font-semibold uppercase tracking-[1px]">
+                    {item.category}
+                  </h4>
+                  <div className="flex items-center gap-1">
+                    <RiCheckboxCircleFill className="text-accent text-xl" />
+                    <p className="text-sm">{item.description}</p>
+                  </div>
+                </div>
+                <Link
+                  href={item.href || "#"}
+                  className="w-[44px] xl:w-[60px] xl:h-[60px] h-[44px] bg-accent text-black text-2xl flex justify-center items-center absolute right-3"
+                >
+                  <RiArrowRightUpLine />
+                </Link>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
